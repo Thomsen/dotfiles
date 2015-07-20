cce2449decca1e2ffb222f4efe3c4ffbed471f5e
Thomsen <changsen4584@163.com>
2015-07-20 20:44:13 +0800

    solved member void-variable yas-snippet-dirs

--- .emacs.d/elpa-init/init-yasnippet.el
+++ .emacs.d/elpa-init/init-yasnippet.el
@@ -1,12 +1,11 @@
 (maybe-require-package 'yasnippet)
 
-;; set custom snippets
-(setq snippets (expand-file-name "~/.emacs.d/snippets"))
-(if (and (file-exists-p snippets) (not (member snippets yas-snippet-dirs)))
- (add-to-list 'yas-snippet-dirs snippets))
-
 (require 'yasnippet)
 (yas/global-mode 1)
 
+;; set custom snippets
+(setq snippets (expand-file-name "~/.emacs.d/snippets"))
+(if (and (file-exists-p snippets) (not (member snippets yas-snippet-dirs)))
+    (add-to-list 'yas-snippet-dirs snippets))
 
 (provide 'init-yasnippet)
